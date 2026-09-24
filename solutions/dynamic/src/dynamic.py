8d47da522f1a738b8b929ea6a78a9c8df7d40e45
George Tservenis <george.tservenis@gmail.com>
2026-09-24 19:25:53 +0200

    feat(dyn): implement comparisons

--- solutions/dynamic/src/dynamic.py
+++ solutions/dynamic/src/dynamic.py
@@ -21,8 +21,18 @@ def compare(op, v1: int, v2: int) -> bool:
     match op:
         case jvm.CmpOpr.Eq:
             return v1 == v2
+        case jvm.CmpOpr.Ne:
+            return v1 != v2
+        case jvm.CmpOpr.Le:
+            return v1 <= v2
+        case jvm.CmpOpr.Lt:
+            return v1 < v2
+        case jvm.CmpOpr.Gt:
+            return v1 > v2
+        case jvm.CmpOpr.Ge:
+            return v1 >= v2
         case _:
-            raise NotImplementedError(f"Unhandled comparation {op!r}")
+            raise NotImplementedError(f"Unhandled comparison {op!r}")
 
 
 def step(bc: jpamb.Bytecode, state: jvmc.State) -> tuple[jvmc.PC, jvmc.State | str]:
